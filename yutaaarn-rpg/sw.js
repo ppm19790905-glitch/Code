8bdf09832b0c844b60c649c3d095b4e0dfcdb762
ppm19790905-glitch <ppm19790905@gmail.com>
2026-08-19 20:26:37 +0900

    Refresh RPG cache for environment polish

--- yutaaarn-rpg/sw.js
+++ yutaaarn-rpg/sw.js
@@ -1,4 +1,4 @@
-const CACHE='yutaaarn-rpg-public-v2';
+const CACHE='yutaaarn-rpg-public-v3';
 const ASSETS=['./','./index.html','./game-core.js','./game-play.js','./game-render.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
